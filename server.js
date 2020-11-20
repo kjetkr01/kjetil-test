f5e6e343ff53ea9d873c952de6a5a6b37060a011
kjetkr01 <55240880+kjetkr01@users.noreply.github.com>
2020-11-20 20:18:11 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ server.set("port", port);
 server.use(express.static("public"));
 server.use(bodyParser.json());
 
-server.get("/", (req,res,next)=>{
-
-  res.status(200).send("Hello World").end();
-
-});
-
 server.listen(server.get("port"), function () {
      console.log("server running", server.get("port"));
 });
