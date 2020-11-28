92129c49b0a05fb998ceb8990d85a66653f85f85
kjetkr01 <kjetil2001@gmail.com>
2020-11-28 20:56:35 +0100

    fix

---
+++ public/scripts/global.js
@@ -0,0 +1,64 @@
+function isGymOpen() {
+
+    const openTime = 6; // 6
+    const closeTime = 23; // 23
+
+    let isOpenMessage = "";
+    let timeLeftMessage = "";
+    let timeLeftString = "";
+
+    let hoursLeft = 0;
+    let minutesLeft = 0;
+
+    const today = new Date();
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+
+    let total = (hours * 60) + minutes;
+
+
+    let testTime = closeTime * 60;
+    let calc = testTime - total;
+
+    if (calc > 0) {
+
+        // open
+
+        calc = calc / 60;
+        hoursLeft = Math.trunc(calc);
+        minutesLeft = Math.round((calc - hoursLeft) * 60);
+
+        if (hoursLeft < 1) {
+            timeLeftString = `${minutesLeft} min`;
+        } else {
+            timeLeftString = `${hoursLeft} t og ${minutesLeft} min`;
+        }
+
+        isOpenMessage = `Treningssenteret er åpent!`;
+        timeLeftMessage = `Stenger om ${timeLeftString}`;
+
+    } else {
+
+        // closed
+
+        testTime = openTime * 60;
+        calc = total - testTime;
+
+        calc = calc / 60;
+        hoursLeft = Math.trunc(24 - calc);
+        minutesLeft = Math.round(((24 - calc) - hoursLeft) * 60);
+
+        if (hoursLeft < 1) {
+            timeLeftString = `${minutesLeft} min`;
+        } else {
+            timeLeftString = `${hoursLeft} t og ${minutesLeft} min`;
+        }
+
+        isOpenMessage = `Treningssenteret er stengt!`;
+        timeLeftMessage = `Åpner om ${timeLeftString}`;
+
+    }
+
+    return { "message": isOpenMessage, "timeLeft": timeLeftMessage };
+}
