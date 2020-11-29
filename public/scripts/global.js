4687e513a82165fecead06c75af0ad95022f28a9
kjetkr01 <kjetil2001@gmail.com>
2020-11-29 18:31:52 +0100

    fix

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,5 +1,82 @@
 function isGymOpen() {
 
+    // new version
+
+    const now = new Date().getTime();
+
+    const today = new Date();
+    const hours = today.getHours();
+
+    const endMonthAndDay = "Dec 8";
+    const endYear = "2022";
+
+    const closeTimeCheck = 23; // close time
+    const openTimeCheck = 6; // open time
+
+    const closeTime = new Date(`${endMonthAndDay}, ${endYear} ${closeTimeCheck}:00:00`).getTime();
+    const openTime = new Date(`${endMonthAndDay}, ${endYear} ${openTimeCheck}:00:00`).getTime();
+
+    let isOpenMessage = "";
+    let timeLeftMessage = "";
+    let timeLeftString = "";
+
+    let hoursLeft = 0;
+    let minutesLeft = 0;
+
+    if (today.getTime() > openTime) {
+
+        isOpenMessage = ``;
+        timeLeftMessage = ``;
+
+    } else {
+
+        if (hours > openTimeCheck && hours < closeTimeCheck) {
+
+            const distance = closeTime - now;
+
+            hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutesLeft = Math.ceil((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+            if (hoursLeft < 1) {
+                timeLeftString = `${minutesLeft} min`;
+            } else if (minutesLeft < 0) {
+                timeLeftString = `${hoursLeft} t`;
+            } else {
+                timeLeftString = `${hoursLeft} t og ${minutesLeft} min`;
+            }
+
+            isOpenMessage = `Treningssenteret er åpent!`;
+            timeLeftMessage = `Stenger om ${timeLeftString}`;
+
+        } else {
+
+            const distance = openTime - now;
+
+            hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            minutesLeft = Math.ceil((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+            if (hoursLeft < 1) {
+                timeLeftString = `${minutesLeft} min`;
+            } else if (minutesLeft < 0) {
+                timeLeftString = `${hoursLeft} t`;
+            } else {
+                timeLeftString = `${hoursLeft} t og ${minutesLeft} min`;
+            }
+
+            isOpenMessage = `Treningssenteret er stengt!`;
+            timeLeftMessage = `Åpner om ${timeLeftString}`;
+        }
+
+    }
+
+
+
+    //
+
+
+
+    /* old version
+
     const openTime = 6; // 6
     const closeTime = 23; // 23
 
@@ -12,7 +89,11 @@ function isGymOpen() {
     let hoursLeft = 0;
     let minutesLeft = 0;
 
-    const hours = today.getHours();
+    let hours = today.getHours();
+
+
+    hours = 1;
+
     const minutes = today.getMinutes();
 
     const total = (hours * 60) + minutes;
@@ -20,9 +101,8 @@ function isGymOpen() {
     let checkTime = closeTime * 60;
     let calc = checkTime - total;
 
-    if (calc > 0) {
-
-        // open
+    if (calc > 0 && hours >= openTime) {
+        console.log("åpent")
 
         calc = calc / 60;
         hoursLeft = Math.trunc(calc);
@@ -40,15 +120,31 @@ function isGymOpen() {
         timeLeftMessage = `Stenger om ${timeLeftString}`;
 
     } else {
-
-        // closed
+        console.log("stengt")
 
         checkTime = openTime * 60;
         calc = total - checkTime;
 
         calc = calc / 60;
-        hoursLeft = Math.trunc(24 - calc);
-        minutesLeft = Math.round(((24 - calc) - hoursLeft) * 60);
+
+        if (hours >= 0) {
+
+            // etter 0
+            hoursLeft = Math.trunc(calc - 24);
+            minutesLeft = Math.round((hoursLeft - (calc - 24)) * 60);
+
+        } else {
+
+            //før 0
+            hoursLeft = Math.trunc(24 - calc);
+            minutesLeft = Math.round(calc - hoursLeft * 60);
+
+        }
+
+
+
+
+        console.log(hoursLeft + ":" + minutesLeft)
 
         if (hoursLeft < 1) {
             timeLeftString = `${minutesLeft} min`;
@@ -60,11 +156,13 @@ function isGymOpen() {
 
         isOpenMessage = `Treningssenteret er stengt!`;
         timeLeftMessage = `Åpner om ${timeLeftString}`;
-
     }
+
+    */
 
     return { "message": isOpenMessage, "timeLeft": timeLeftMessage };
 }
+
 
 function partOfDayMessage(firstName) {
 
