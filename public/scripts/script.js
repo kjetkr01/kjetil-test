f6db29e5bc222842e749d8fc6101f3ab9f6ab623
kjetkr01 <kjetil2001@gmail.com>
2020-11-29 19:17:23 +0100

    fix

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,3 +18,35 @@ async function callServerAPI(body, url) {
     return { "response": response, "data": data };
 
 }
+
+
+function loadPage(page) {
+
+    const pages = ["index", "login", "access"];
+    let redirect = false;
+
+    for (let i = 0; i < pages.length; i++) {
+
+        if (page === pages[i]) {
+            console.log("exists");
+            redirect = true;
+        }
+
+    }
+
+    if (redirect) {
+
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.documentElement.innerHTML =
+                    this.responseText;
+            }
+        };
+        xhttp.open("GET", `${page}.html`, true);
+        xhttp.send();
+
+    }else{
+        console.log("Page does not exist!");
+    }
+}
