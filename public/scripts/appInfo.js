0f735b48de6842bcbbb3a912cc93fb30fefd6cea
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-03-13 19:12:13 +0100

    fix

--- public/scripts/appInfo.js
+++ public/scripts/appInfo.js
@@ -2,7 +2,7 @@ const applicationName = `Treningstatistikken`;
 
 const vMajorVersion = 0;
 const vMinorVersion = 0;
-const vRevision = 2;
+const vRevision = 3;
 
 const applicationVersionNumber = `${vMajorVersion}.${vMinorVersion}.${vRevision}`;
 const applicationVersionState = `alpha`;
