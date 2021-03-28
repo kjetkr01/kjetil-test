5dfbb933ef5140f99e03b82b1388d654c2f9970e
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-03-28 18:19:14 +0200

    fix

--- public/scripts/appInfo.js
+++ public/scripts/appInfo.js
@@ -7,7 +7,7 @@ const application = {
         state: "alpha",
         major: 3, // major++ = minor = 0 && revision = 0
         minor: 5, // minor++ = revision = 0
-        revision: 11,
+        revision: 12,
     },
     lastUpdated: {
         day: "28",
