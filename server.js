90727f78df773e19fc83813cd1f1f6e3b69373a3
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-04-29 11:47:42 +0200

    fix

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ const application = {
           state: "alpha 84%",
           major: 4, // major++ = minor = 0 && revision = 0
           minor: 3, // minor++ = revision = 0
-          revision: 18,
+          revision: 19,
      },
      lastUpdated: {
           day: "29",
