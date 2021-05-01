12653fe9993ad6e4a3a96a1498c8cada713225d7
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-05-01 18:04:07 +0200

    fix

--- public/ts_application.js
+++ public/ts_application.js
@@ -9,7 +9,7 @@
             state: "alpha 84%",
             major: 4, // major++ = minor = 0 && revision = 0
             minor: 3, // minor++ = revision = 0
-            revision: 72,
+            revision: 75,
         },
         lastUpdated: {
             day: "01",
