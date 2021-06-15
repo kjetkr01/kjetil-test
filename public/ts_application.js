8f72afa1eca059f2bcfe75c336da901f59f7da33
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-06-15 15:14:21 +0200

    fix

--- public/ts_application.js
+++ public/ts_application.js
@@ -16,7 +16,7 @@
             major: 6,
             minor: 3,
             revision: 24,
-            buildnumber: 971,
+            buildnumber: 972,
         },
         lastUpdated: {
             day: upd[0],
