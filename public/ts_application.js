c3f8d7002f638e2e3c9c5a106a1a383c11c8fae2
Kjetil Kristiansen <kjetil2001@gmail.com>
2021-06-24 22:34:37 +0200

    Fix

--- public/ts_application.js
+++ public/ts_application.js
@@ -4,6 +4,11 @@
         name: "Treningstatistikken",
         logoURL: "images/placeholder_logo.svg",
         changeLog: {
+            "6.3.27.998": {
+                "txt":
+                    ["Feilrettinger"],
+                "date": "24.06.2021"
+            },
             "6.3.27.997": {
                 "txt":
                     ["Lagt til 'loading gif'",
